04d2ed9c16d7d065556de504ccb2dbb2427b6d8d
Niek Janssen <niek.janssen01@gmail.com>
2017-11-14 17:24:58 +0100

    Small start for compiler/runtime distribution. Mainly transfer commit.

---
+++ test/schemer.spec.ts
@@ -0,0 +1,8 @@
+import {ApiOneCompiler} from '../src/compiler/compiler';
+
+describe('The type schemer', ()=> {
+    it('should load the config file', ()=>{
+        const compiler = new ApiOneCompiler();
+        compiler.loadConfigFromFile('apioneconfig.json');
+    })
+})
