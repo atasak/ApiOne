2ed5f574174da099ca37ed434bc4e4597891766e
Niek Janssen <niek.janssen01@gmail.com>
2017-12-25 16:11:58 +0100

    some cleanup, groundwork and added travis

--- src/runtime/contentwrappers/dictwrapper.d.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractWrapper } from './abstractwrapper';
-export declare class DictWrapper<T> extends AbstractWrapper<T> {
-    constructor();
-    _get(): T;
-    $get(): Promise<T>;
-    set(t: T): void;
-}
--- src/runtime/contentwrappers/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { ClassWrapper } from './classwrapper';
-export { DictWrapper } from './dictwrapper';
-export { ListWrapper } from './listwrapper';
-export { VarWrapper } from './varwrapper';
