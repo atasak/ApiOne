6a9f43dd089d3e431bf43e42605c614c917a106b
Niek Janssen <niek.janssen01@gmail.com>
2018-01-05 23:04:25 +0100

    test coverage working with all files, not only tested

--- src/util/id.spec.ts
+++ src/util/id.spec.ts
@@ -10,8 +10,10 @@ describe('The Id module', () => {
         idFactory = new IdFactory(testMask);
     });
 
+    it('should ')
+
     it('should create an id according to a mask', () => {
-        for (let i = 0; i < 100; i++) {
+        for (let i = 0; i < 1000; i++) {
             const id = idFactory.id();
             expect(id[0]).to.equal('0');
             expect(id[1]).not.to.equal('0');
